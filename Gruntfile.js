e4bcc7829cf5f20ef39b7b8b3740759f6a21c4bf
Ed Sanders <ejsanders@gmail.com>
2016-09-29 17:06:27 -0400

    Introduce linters

---
+++ Gruntfile.js
@@ -0,0 +1,37 @@
+/*!
+ * Grunt file
+ */
+
+module.exports = function ( grunt ) {
+	grunt.loadNpmTasks( 'grunt-eslint' );
+	grunt.loadNpmTasks( 'grunt-jsonlint' );
+	grunt.loadNpmTasks( 'grunt-stylelint' );
+
+	grunt.initConfig( {
+		pkg: grunt.file.readJSON( 'package.json' ),
+		eslint: {
+			dev: [
+				'*.js'
+			]
+		},
+		jsonlint: {
+			dev: [
+				'*.json'
+			]
+		},
+		stylelint: {
+			dev: [
+				'*.css'
+			]
+		},		watch: {
+			files: [
+				'.{eslint.json}',
+				'<%= eslint.dev %>'
+			],
+			tasks: '_test'
+		}
+	} );
+
+	grunt.registerTask( 'lint', [ 'eslint', 'jsonlint', 'stylelint' ] );
+	grunt.registerTask( 'default', 'lint' );
+};
