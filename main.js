0ebe719dcde85ca43a60159688a37fc8165f7339
Ed Sanders <ejsanders@gmail.com>
2017-05-08 14:29:56 +0100

    build: Update linters and remove exceptions

--- main.js
+++ main.js
@@ -64,11 +64,13 @@ $( function () {
 						$( '<a>' )
 							.attr( 'href', '#' )
 							.text( 'x' )
+							// eslint-disable-next-line no-use-before-define
 							.click( onDeleteClick ),
 						'] ',
 						$( '<a>' )
 							.attr( 'href', '#' )
 							.text( name )
+							// eslint-disable-next-line no-use-before-define
 							.click( onLoadClick ),
 						' ',
 						$( '<code>' ).text( savedStates[ name ].wikitext.substr( 0, 40 ) + '...' )
